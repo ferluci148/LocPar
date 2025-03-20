71e781ff526f9bc8554131ed0f963b49d85626e9
ferluci148 <fluciagabriel@gmail.com>
2025-03-20 21:46:12 +0100

    Primer commit

---
+++ obtenerDatosJson.js
@@ -0,0 +1,150 @@
+
+            fincas = [
+                {
+                    "id": "1",
+                    "descripcion": "La Calabaza",
+                    "poligono":"538",
+                    "parcela":"6704",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "44a",
+                    "cepas":"800",
+                    "edad":"18",
+                    "latitud":"41.67913834361563",
+                    "longitud":"-3.5817146301269",
+                    "recinto":"new google.maps.LatLng(41.67765188586432, -3.5826855897903442),new google.maps.LatLng(41.6791423501939, -3.581693172454834),new google.maps.LatLng(41.679154369927204, -3.5819238424301147),new google.maps.LatLng(41.6777400325646, -3.582921624183655)"
+                },
+                {
+    
+ 
+                    "id": "2",  
+                    "descripcion": "Tia Feliciana",
+                     "poligono":"538",
+                    "parcela":"16441",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "50a",
+                    "cepas":"1000",
+                    "edad":"18",
+                    "latitud":"41.679514960882756",
+                    "longitud":"-3.5768544673919677",
+                    "recinto":" new google.maps.LatLng(41.679226488279944, -3.576875925064087), new google.maps.LatLng(41.681886797701594, -3.5767364501953125), new google.maps.LatLng(41.68191083614077, -3.576875925064087), new google.maps.LatLng(41.67923450142526, -3.5770046710968018), new google.maps.LatLng(41.679226488279944, -3.576875925064087)"
+                },
+                {
+                    "id":"3",
+                    "descripcion": "Botijo",
+                     "poligono":"538",
+                    "parcela":"16636",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "18a",
+                    "cepas":"400",
+                    "edad":"30",
+                    "latitud":"41.67582882476762",
+                    "longitud":"-3.5828572511672",
+                    "recinto":"new google.maps.LatLng(41.67582882476762, -3.5828572511672974),new google.maps.LatLng(41.67690263403767, -3.581993579864502),new google.maps.LatLng(41.67682650635855, -3.5819023847579956),new google.maps.LatLng(41.675748689029064, -3.5827231407165527)"
+                } ,
+                {
+                    "id":"4",
+                    "descripcion": "Natalia",
+                     "poligono":"538",
+                    "parcela":"6673",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "7a",
+                    "cepas":"200",
+                    "edad":"30",
+                    "latitud":"41.675844851903335",
+                    "longitud":"-3.57942402362823",
+                    "recinto":"new google.maps.LatLng(41.675844851903335, -3.579424023628235),new google.maps.LatLng(41.67570862112238, -3.5785388946533203),new google.maps.LatLng(41.67560444444827, -3.5786139965057373),new google.maps.LatLng(41.675756702607416, -3.5795044898986816),new google.maps.LatLng(41.675844851903335, -3.579424023628235)"
+                } ,
+                {
+                    "id":"5",
+                    "descripcion": "Chulo",
+                     "poligono":"538",
+                    "parcela":"6661",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "32a",
+                    "cepas":"600",
+                    "edad":"30",
+                    "latitud":"41.67519975653752",
+                    "longitud":" -3.5808938741683",
+                    "recinto":"new google.maps.LatLng(41.67519975653752, -3.580893874168396),new google.maps.LatLng(41.675131640500396, -3.5807275772094727),new google.maps.LatLng(41.676610142989475, -3.5795098543167114),new google.maps.LatLng(41.67668226419332, -3.5796386003494263),new google.maps.LatLng(41.67519975653752, -3.580893874168396)"
+                } ,
+                {
+                    "id":"6",
+                    "descripcion": "Alejandro",
+                     "poligono":"538",
+                    "parcela":"6508/6561",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "13a",
+                    "cepas":"600",
+                    "edad":"30/12",
+                    "latitud":"41.681826701564354",
+                    "longitud":" -3.58143568038940",
+                    "recinto":"new google.maps.LatLng(41.681826701564354, -3.5814356803894043),new google.maps.LatLng(41.68171051553992, -3.5814356803894043),new google.maps.LatLng(41.68123375000158, -3.5807543992996216),new google.maps.LatLng(41.68164240639355, -3.5802286863327026),new google.maps.LatLng(41.68187077207048, -3.580314517021179),new google.maps.LatLng(41.68201500260676, -3.5804110765457153),new google.maps.LatLng(41.681662438502904, -3.580668568611145),new google.maps.LatLng(41.68163038712496, -3.5807275772094727),new google.maps.LatLng(41.68179064385508, -3.581167459487915),new google.maps.LatLng(41.681826701564354, -3.5813337564468384),new google.maps.LatLng(41.681826701564354, -3.5814356803894043)"
+                }  ,
+                {
+                    "id":"7",
+                    "descripcion": "Cañada Baltasar",
+                     "poligono":"538",
+                    "parcela":"16555",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "26a",
+                    "cepas":"600",
+                    "edad":"30/12",
+                    "latitud":"41.682583908791834",
+                    "longitud":"-3.58318448066711",
+                    "recinto":"new google.maps.LatLng(41.682583908791834, -3.5831844806671143),new google.maps.LatLng(41.68240362216493, -3.5829484462738037),new google.maps.LatLng(41.6831327783017, -3.5818004608154297),new google.maps.LatLng(41.683192873219156, -3.5818326473236084),new google.maps.LatLng(41.683020600972306, -3.582170605659485),new google.maps.LatLng(41.68288839172589, -3.5825514793395996),new google.maps.LatLng(41.682583908791834, -3.5831844806671143)"
+                } ,
+                {
+                    "id":"8",
+                    "descripcion": "El Hoyo",
+                     "poligono":"528",
+                    "parcela":"859",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "2h 21 a",
+                    "cepas":"6000",
+                    "edad":"20",
+                    "latitud":"41.69996116816865",
+                    "longitud":"-3.5857272148132",
+                    "recinto":"new google.maps.LatLng(41.69996116816865, -3.5857272148132324),new google.maps.LatLng(41.699816977864145, -3.58479380607605),new google.maps.LatLng(41.69754994335504, -3.584718704223633),new google.maps.LatLng(41.697373704109054, -3.585684299468994),new google.maps.LatLng(41.69996116816865, -3.5857272148132324)"
+                } ,
+                {
+                    "id":"9",
+                    "descripcion": "Carrahontoria",
+                     "poligono":"521",
+                    "parcela":"564",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "2h 28 a",
+                    "cepas":"6000",
+                    "edad":"20",
+                    "latitud":"41.70181158171184",
+                    "longitud":"-3.5905981063842",
+                    "recinto":"new google.maps.LatLng(41.70181158171184, -3.5905981063842773),new google.maps.LatLng(41.70229219989489, -3.5897719860076904),new google.maps.LatLng(41.7038381640215, -3.5916495323181152),new google.maps.LatLng(41.70270072252606, -3.592379093170166),new google.maps.LatLng(41.70181158171184, -3.5905981063842773)"
+                } ,
+                {
+                    "id":"10",
+                    "descripcion": "Cabezas",
+                     "poligono":"522",
+                    "parcela":"599",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "1h 45a",
+                    "cepas":"4000",
+                    "edad":"20",
+                    "latitud":"41.70195576754394",
+                    "longitud":"-3.6040198802948",
+                    "recinto":"new google.maps.LatLng(41.70195576754394, -3.6040198802948),new google.maps.LatLng(41.70351776000208, -3.604738712310791),new google.maps.LatLng(41.70308521204329, -3.605661392211914),new google.maps.LatLng(41.702860926770974, -3.605586290359497),new google.maps.LatLng(41.70262863048561, -3.605393171310425),new google.maps.LatLng(41.702348271782235, -3.605393171310425),new google.maps.LatLng(41.70197178817199, -3.605167865753174),new google.maps.LatLng(41.70190770563581, -3.6047279834747314),new google.maps.LatLng(41.70185163336428, -3.6044061183929443),new google.maps.LatLng(41.70195576754394, -3.6040198802948)"
+                } ,
+                {
+                    "id":"11",
+                    "descripcion": "Cascorral",
+                     "poligono":"523",
+                    "parcela":"626",
+                    "plantacion": "Tinto temparnillo",
+                    "superficie": "1h 40a",
+                    "cepas":"4000",
+                    "edad":"20",
+                    "latitud":"41.699224192104595",
+                    "longitud":"-3.60842943191528",
+                    "recinto":"new google.maps.LatLng(41.699224192104595, -3.608429431915283),new google.maps.LatLng(41.699736871999754, -3.608182668685913),new google.maps.LatLng(41.6998089672822, -3.6078929901123047),new google.maps.LatLng(41.69948053256307, -3.6064445972442627),new google.maps.LatLng(41.698735540275266, -3.606584072113037),new google.maps.LatLng(41.698831668797354, -3.607689142227173),new google.maps.LatLng(41.69913607483603, -3.6078929901123047),new google.maps.LatLng(41.699224192104595, -3.608429431915283)"
+                } 
+            
+  ];
+     
